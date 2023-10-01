d65a98a2c3e2a05311de91fb7eed32dd0c6fc983
Kreylanc <devjanpun@gmail.com>
2023-10-01 10:23:27 +0100

    Schema: Add file table and query

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 
@@ -30,6 +30,17 @@ export const appRouter = router({
     }
 
     return { success: true };
+  }),
+
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    // query to fetch file where the userId matches
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
   }),
 });
 // Export type router type signature,
