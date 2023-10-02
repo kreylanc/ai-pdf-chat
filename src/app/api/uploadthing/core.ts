6e7403a10e502aa0598de669ef4f47f984dc96cc
Kreylanc <devjanpun@gmail.com>
2023-10-02 15:40:19 +0100

    Fix: img to pdf

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,7 +6,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
